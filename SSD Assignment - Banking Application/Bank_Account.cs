4a68c79a0d383e583aad99930225a86ea2a0af0b
A7MEDGH <a.alnoumani99@gmail.com>
2023-09-01 22:20:20 +0100

    Add project files.

---
+++ SSD Assignment - Banking Application/Bank_Account.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_Application
+{
+    public abstract class Bank_Account
+    {
+
+        public string accountNo; // Account Number is not sensitive, so we're not encrypting it
+        private byte[] encryptedBalance;
+        public string name, address_line_1, address_line_2, address_line_3, town;
+        public double balance;
+
+        private static byte[] encryptionKey = Encoding.UTF8.GetBytes("A_16_BYTE_KEY!!"); // AES-128 requires 16 bytes key
+
+        protected double GetDecryptedBalance()
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = encryptionKey;
+                byte[] decrypted = Decrypt(aes, encryptedBalance);
+                return BitConverter.ToDouble(decrypted, 0);
+            }
+        }
+
+        protected void SetEncryptedBalance(double balance)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = encryptionKey;
+                encryptedBalance = Encrypt(aes, BitConverter.GetBytes(balance));
+            }
+        }
+
+        private byte[] Encrypt(Aes aes, byte[] data)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(data, 0, data.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+
+        private byte[] Decrypt(Aes aes, byte[] data)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(data, 0, data.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+        public abstract double getAvailableFunds();
+        public void lodge(double amount)
+        {
+            balance += amount;
+        }
+        public abstract bool withdraw(double amount);
+
+        ~Bank_Account() // Destructor to clear sensitive data from memory
+        {
+            encryptedBalance = null;
+            GC.Collect(); // Manual garbage collection
+        }
+    }
+}
---
+++ SSD Assignment - Banking Application/Data_Access_Layer.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using System.Diagnostics.PerformanceCounter;
+namespace Banking_Application
+{
+    public class Data_Access_Layer
+    {
+
+        private List<Bank_Account> accounts;
+        public static String databaseName = "Banking Database.db";
+        private static Data_Access_Layer instance = new Data_Access_Layer();
+
+        private const string hmacKey = "h8Kjs2C4d87NxePmJzCf3Xn8q6S5r4tL";
+        private byte[] HMACKey = new byte[32];
+        private Data_Access_Layer()
+        {
+            accounts = new List<Bank_Account>();
+        }
+
+        public static Data_Access_Layer getInstance()
+        {
+            return instance;
+        }
+
+        private SqliteConnection getDatabaseConnection()
+        {
+
+            String databaseConnectionString = new SqliteConnectionStringBuilder()
+            {
+                DataSource = Data_Access_Layer.databaseName,
+                Mode = SqliteOpenMode.ReadWriteCreate
+            }.ToString();
+
+            return new SqliteConnection(databaseConnectionString);
+
+        }
+
+        private string ComputeHMACSHA256(string data, string HMACKey)
+        {
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA256(Encoding.UTF8.GetBytes(HMACKey)))
+            {
+                var hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+
+        private void LogAction(string user, string action, string dataItem)
+        {
+            System.IO.File.AppendAllText("log.txt", $"{DateTime.Now}: User '{user}' performed action '{action}' on data item '{dataItem}'\n");
+        }
+
+        private long GetAvailableDiskSpace()
+        {
+            var drive = new System.IO.DriveInfo(System.AppDomain.CurrentDomain.BaseDirectory);
+            return drive.AvailableFreeSpace;
+        }
+
+        private long GetAvailableMemory()
+        {
+            using (var pc = new System.Diagnostics.PerformanceCounter("Memory", "Available Bytes"))
+            {
+                return (long)pc.NextValue();
+            }
+        }
+
+        private float GetCurrentCpuUsage()
+        {
+            using (var pc = new System.Diagnostics.PerformanceCounter("Processor", "% Processor Time", "_Total", true))
+            {
+                return pc.NextValue();  // Returns in percentage
+            }
+        }
+        private void initialiseDatabase()
+        {
+            using (var connection = getDatabaseConnection())
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                    CREATE TABLE IF NOT EXISTS Bank_Accounts(    
+                        accountNo TEXT PRIMARY KEY,
+                        name TEXT NOT NULL,
+                        address_line_1 TEXT,
+                        address_line_2 TEXT,
+                        address_line_3 TEXT,
+                        town TEXT NOT NULL,
+                        balance REAL NOT NULL,
+                        accountType INTEGER NOT NULL,
+                        overdraftAmount REAL,
+                        interestRate REAL
+                    ) WITHOUT ROWID
+                ";
+
+                command.ExecuteNonQuery();
+                
+            }
+        }
+
+        public void loadBankAccounts()
+        {
+            if (!File.Exists(Data_Access_Layer.databaseName))
+                initialiseDatabase();
+            else
+            {
+
+                using (var connection = getDatabaseConnection())
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = "SELECT * FROM Bank_Accounts";
+                    SqliteDataReader dr = command.ExecuteReader();
+                    
+                    while(dr.Read())
+                    {
+
+                        int accountType = dr.GetInt16(7);
+
+                        if(accountType == Account_Type.Current_Account)
+                        {
+                            Current_Account ca = new Current_Account();
+                            ca.accountNo = dr.GetString(0);
+                            ca.name = dr.GetString(1);
+                            ca.address_line_1 = dr.GetString(2);
+                            ca.address_line_2 = dr.GetString(3);
+                            ca.address_line_3 = dr.GetString(4);
+                            ca.town = dr.GetString(5);
+                            ca.balance = dr.GetDouble(6);
+                            ca.overdraftAmount = dr.GetDouble(8);
+                            accounts.Add(ca);
+                        }
+                        else
+                        {
+                            Savings_Account sa = new Savings_Account();
+                            sa.accountNo = dr.GetString(0);
+                            sa.name = dr.GetString(1);
+                            sa.address_line_1 = dr.GetString(2);
+                            sa.address_line_2 = dr.GetString(3);
+                            sa.address_line_3 = dr.GetString(4);
+                            sa.town = dr.GetString(5);
+                            sa.balance = dr.GetDouble(6);
+                            sa.interestRate = dr.GetDouble(9);
+                            accounts.Add(sa);
+                        }
+
+
+                    }
+
+                }
+
+            }
+        }
+
+        public String addBankAccount(Bank_Account ba)
+        {
+            // Check disk space before saving
+            if (GetAvailableDiskSpace() < 1024) // Assuming you need at least 1KB free space
+                throw new Exception("Not enough disk space!");
+
+            // Check available memory before doing operations
+            if (GetAvailableMemory() < 1024 * 100) // Assuming you need at least 100KB free memory
+                throw new Exception("Not enough memory!");
+
+            // Check CPU usage
+            if (GetCurrentCpuUsage() > 80) // Assuming you'd like at least 20% CPU free
+                throw new Exception("High CPU usage!");
+
+            using (var connection = getDatabaseConnection())
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = @"
+                    INSERT INTO Bank_Accounts(accountNo, name, address_line_1, address_line_2, address_line_3, town, balance, accountType, overdraftAmount, interestRate) 
+                    VALUES (@accountNo, @name, @address_line_1, @address_line_2, @address_line_3, @town, @balance, @accountType, @overdraftAmount, @interestRate)";
+
+                command.Parameters.AddWithValue("@accountNo", ba.accountNo);
+                command.Parameters.AddWithValue("@name", ba.name);
+                command.Parameters.AddWithValue("@address_line_1", ba.address_line_1);
+                command.Parameters.AddWithValue("@address_line_2", ba.address_line_2);
+                command.Parameters.AddWithValue("@address_line_3", ba.address_line_3);
+                command.Parameters.AddWithValue("@town", ba.town);
+                command.Parameters.AddWithValue("@balance", ba.balance);
+                command.Parameters.AddWithValue("@accountType", ba.GetType() == typeof(Current_Account) ? 1 : 2);
+                command.Parameters.AddWithValue("@overdraftAmount", ba.GetType() == typeof(Current_Account) ? ((Current_Account)ba).overdraftAmount : DBNull.Value);
+                command.Parameters.AddWithValue("@interestRate", ba.GetType() == typeof(Savings_Account) ? ((Savings_Account)ba).interestRate : DBNull.Value);
+
+                command.ExecuteNonQuery();
+            }
+
+            LogAction("System", "Add", ba.accountNo);  // Assuming 'System' user for now
+
+            return ba.accountNo;
+        }
+
+        public Bank_Account findBankAccountByAccNo(String accNo) 
+        { 
+        
+            foreach(Bank_Account ba in accounts)
+            {
+
+                if (ba.accountNo.Equals(accNo))
+                {
+                    return ba;
+                }
+
+            }
+
+            return null; 
+        }
+
+        public bool closeBankAccount(String accNo) 
+        {
+
+            Bank_Account toRemove = null;
+            
+            foreach (Bank_Account ba in accounts)
+            {
+
+                if (ba.accountNo.Equals(accNo))
+                {
+                    toRemove = ba;
+                    break;
+                }
+
+            }
+
+            if (toRemove == null)
+                return false;
+            else
+            {
+                accounts.Remove(toRemove);
+
+                using (var connection = getDatabaseConnection())
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = "DELETE FROM Bank_Accounts WHERE accountNo = '" + toRemove.accountNo + "'";
+                    command.ExecuteNonQuery();
+
+                }
+
+                return true;
+            }
+
+        }
+
+        public bool lodge(String accNo, double amountToLodge)
+        {
+
+            Bank_Account toLodgeTo = null;
+
+            foreach (Bank_Account ba in accounts)
+            {
+
+                if (ba.accountNo.Equals(accNo))
+                {
+                    ba.lodge(amountToLodge);
+                    toLodgeTo = ba;
+                    break;
+                }
+
+            }
+
+            if (toLodgeTo == null)
+                return false;
+            else
+            {
+
+                using (var connection = getDatabaseConnection())
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = "UPDATE Bank_Accounts SET balance = " + toLodgeTo.balance + " WHERE accountNo = '" + toLodgeTo.accountNo + "'";
+                    command.ExecuteNonQuery();
+
+                }
+
+                return true;
+            }
+
+        }
+
+        public bool withdraw(String accNo, double amountToWithdraw)
+        {
+
+            Bank_Account toWithdrawFrom = null;
+            bool result = false;
+
+            foreach (Bank_Account ba in accounts)
+            {
+
+                if (ba.accountNo.Equals(accNo))
+                {
+                    result = ba.withdraw(amountToWithdraw);
+                    toWithdrawFrom = ba;
+                    break;
+                }
+
+            }
+
+            if (toWithdrawFrom == null || result == false)
+                return false;
+            else
+            {
+
+                using (var connection = getDatabaseConnection())
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = "UPDATE Bank_Accounts SET balance = " + toWithdrawFrom.balance + " WHERE accountNo = '" + toWithdrawFrom.accountNo + "'";
+                    command.ExecuteNonQuery();
+
+                }
+
+                return true;
+            }
+
+        }
+
+    }
+}
